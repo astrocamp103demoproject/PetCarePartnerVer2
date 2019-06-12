394fffd9ffce235b4715f9a79ccc36f083c28b0f
Eric Syu <xeriok02390@gmail.com>
2019-06-12 10:35:37 +0800

    add_search index and kaminari

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times { 
+    email = Faker::Internet.email
+    name = Faker::Name.unique.name
+        Sitter.create(name: name ,email: email, password: '123456', slogan:'發發發', address:'高雄市大寮區', price:'999')
+    
+}
