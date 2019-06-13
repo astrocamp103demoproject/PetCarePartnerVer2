55797043bd78e71bb359bc0a78af2f738914a550
Eric Syu <xeriok02390@gmail.com>
2019-06-13 14:59:28 +0800

    透過pet_count搜尋pet_limit範圍

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,16 +5,12 @@ class SearchesController < ApplicationController
   def update
     #1.用寵物數量找sitter
     # @sitters = Sitter.all.page(params[:page]).per(10)
-    @pet_limit = params[:xs_count].to_i + params[:s_count].to_i + params[:m_count].to_i + params[:l_count].to_i
-    @result = Sitter.where(pet_limit: @pet_limit).where(address: location_code).page(params[:page]).per(10)
-
+    pet_count
+    @result = Sitter.where("pet_limit >= #{@pet_count}").where(address: location_code).page(params[:page]).per(10)
+    
     
     #2.sitter找到後去booking_date
-    puts "-------------------"
-    puts params
-    puts location_code
-    puts @result
-    puts "-------------------"
+    
   end
 
   private 
@@ -22,12 +18,14 @@ class SearchesController < ApplicationController
     if (params[:city_id]=="縣市" || params[:township_id] == "")
       city = TaiwanCity.get("01000")
       township = TaiwanCity.get("01100")
-    
     else
       city = TaiwanCity.get(params[:city_id])
       township = TaiwanCity.get(params[:township_id])
-      
     end
     @location = city + township
+  end
+
+  def pet_count
+    @pet_count = params[:xs_count].to_i + params[:s_count].to_i + params[:m_count].to_i + params[:l_count].to_i
   end
 end
