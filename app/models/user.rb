17a6bcdd533b6654785f1648360a606b9738fa88
evansun <pipltbtb@gmail.com>
2019-06-13 23:20:06 +0800

    調整profile畫面 ＆ action權限

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  
   has_many :pets
   has_many :orders
 end
