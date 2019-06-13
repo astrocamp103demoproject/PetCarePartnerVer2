e73a7d8c8a9bcb6452e2eba5ceee45fba8b15613
Eric Syu <xeriok02390@gmail.com>
2019-06-13 12:29:48 +0800

    可以同搜尋縣市跟寵物數量

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,11 +6,28 @@ class SearchesController < ApplicationController
     #1.用寵物數量找sitter
     # @sitters = Sitter.all.page(params[:page]).per(10)
     @pet_limit = params[:xs_count].to_i + params[:s_count].to_i + params[:m_count].to_i + params[:l_count].to_i
-    @result = Sitter.where(pet_limit: @pet_limit).where(params[:address]).page(params[:page]).per(10)
+    @result = Sitter.where(pet_limit: @pet_limit).where(address: location_code).page(params[:page]).per(10)
+
+    
     #2.sitter找到後去booking_date
     puts "-------------------"
     puts params
-    
+    puts location_code
+    puts @result
     puts "-------------------"
+  end
+
+  private 
+  def location_code
+    if (params[:city_id]=="縣市" || params[:township_id] == "")
+      city = TaiwanCity.get("01000")
+      township = TaiwanCity.get("01100")
+    
+    else
+      city = TaiwanCity.get(params[:city_id])
+      township = TaiwanCity.get(params[:township_id])
+      
+    end
+    @location = city + township
   end
 end
