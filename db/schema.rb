767369de483356dfc76b30d13283eb9a4e262f56
LucaKao <lars10192002@gmail.com>
2019-06-14 14:24:14 +0800

    1.add_role_to_users.rb 2.add before_action :authenticate_user! to SearchesController

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_145621) do
+ActiveRecord::Schema.define(version: 2019_06_14_062202) do
 
   create_table "booking_dates", force: :cascade do |t|
     t.integer "sitter_id"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_145621) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "role", default: "user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
