b4a8d6dd7573c32cf560c15e52279cf85b234b50
Eric Syu <xeriok02390@gmail.com>
2019-06-15 21:04:37 +0800

    fix vaildtoken and remove date_change

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,23 +1,20 @@
 require 'date'
 class SearchesController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!, except: [:index, :show]
 
   def show  
     @result = Sitter.where("pet_limit >= ?",pet_count).where(address: location_code).page(params[:page]).per(10)
   end
   def update
-    #1.用寵物數量找sitter
-    # @sitters = Sitter.all.page(params[:page]).per(10)
-    
     # if(params[:Drop_Off]=="") && (params[:Pick_Up]=="")
       @result = Sitter.where("pet_limit >= ?",pet_count).where(address: location_code).page(params[:page]).per(10) 
     # else 
     #   @result = Sitter.joins(:booking_dates).where(date: date_change(params[:Drop_Off])..date_change(params[:Pick_Up])).page(params[:page]).per(10)
       # @a = BookingDate.group(:sitter_id).having(date: date_change(params[:Drop_Off])..date_change(params[:Pick_Up])) 
     # end
-
-    # byebug
+    
+    
     # byebug
     
     #2.sitter找到後去booking_date
@@ -27,8 +24,8 @@ class SearchesController < ApplicationController
   private 
   def location_code
     if (params[:city_id]=="縣市" || params[:township_id] == "")
-      city = TaiwanCity.get("01000")
-      township = TaiwanCity.get("01100")
+      city = TaiwanCity.get("")
+      township = TaiwanCity.get("")
     else
       city = TaiwanCity.get(params[:city_id])
       township = TaiwanCity.get(params[:township_id])
@@ -40,8 +37,4 @@ class SearchesController < ApplicationController
     params[:xs_count].to_i + params[:s_count].to_i + params[:m_count].to_i + params[:l_count].to_i
   end
 
-  def date_change(day = "24/11/2018")
-    day1 = day.split("/")
-    DateTime.new(day1[2].to_i,day1[1].to_i,day1[0].to_i).strftime("%b %d,%Y")
-  end
 end
