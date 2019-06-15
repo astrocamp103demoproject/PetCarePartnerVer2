cceee48f977fcc5ce2048e8d6e6af9ec6c328528
evansun <pipltbtb@gmail.com>
2019-06-15 16:50:17 +0800

    adjust order routes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :update]
   def show  
     pet_count
     @result = Sitter.where("pet_limit >= #{@pet_count}").where(address: location_code).page(params[:page]).per(10)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_062202) do
+ActiveRecord::Schema.define(version: 2019_06_14_080256) do
 
   create_table "booking_dates", force: :cascade do |t|
     t.integer "sitter_id"
