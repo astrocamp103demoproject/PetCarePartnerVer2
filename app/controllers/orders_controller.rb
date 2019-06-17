1afdd3532dbcb341d2c1315f264bf4124ed32884
evansun <pipltbtb@gmail.com>
2019-06-17 21:29:57 +0800

    新增訂單同時寫入booking_date (未完成，只寫入頭＆尾日)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @booking_date = BookingDate.new
     @sitter = session[:current_sitter].to_h
     @drop = Date.strptime(session[:drop_off], '%m/%d/%Y')
     @pick = Date.strptime(session[:pick_up], '%m/%d/%Y')
@@ -14,7 +15,23 @@ class OrdersController < ApplicationController
   
   def create
     @order = Order.new(order_params)
+    @sitter = session[:current_sitter].to_h
+    @drop = Date.strptime(session[:drop_off], '%m/%d/%Y')
+    @pick = Date.strptime(session[:pick_up], '%m/%d/%Y')
+    
+    # @booking_date = BookingDate.new
+    @booking_date_drop = BookingDate.new
+    @booking_date_pick = BookingDate.new
+    #@pick - @drop 剩餘天數
+    #迴圈跑剩餘天數
+    #[].push {date: @drop+1}
+
     if @order.save
+      # @booking_date.update(sitter_id: @sitter['id'])
+      @booking_date_drop.update(sitter_id: @sitter['id'], date: @drop, available: false)
+      @booking_date_pick.update(sitter_id: @sitter['id'], date: @pick, available: false)
+      
+      # BookingDate.where(sitter_id: @sitter['id']).update(date: @drop..@pick, :available => false)
       redirect_to user_orders_path, notice:'成功下訂！'
     else
       render :new
@@ -41,4 +58,7 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:user_id, :sitter_id, :drop_off, :pick_up, :status, :note)
   end
+  # def booking_date_params
+  #   params.require(:booking_date).permit(:sitter_id, :date, :avaliable)
+  # end
 end
