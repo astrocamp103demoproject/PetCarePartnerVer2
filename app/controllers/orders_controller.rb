adbdcc35ae576bb4074065634c73f3bea928348c
LucaKao <lars10192002@gmail.com>
2019-06-19 16:44:02 +0800

    [pay] init braintree v2 api

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
+  include Payable
   before_action :authenticate_user!
+
   def index
     @orders = current_user.orders.all
   end
@@ -11,6 +13,8 @@ class OrdersController < ApplicationController
     @drop = Date.strptime(session[:drop_off], '%m/%d/%Y')
     @pick = Date.strptime(session[:pick_up], '%m/%d/%Y')
     @total = (@pick - @drop).to_i * @sitter['price']
+    @token = gateway.client_token.generate
+    
   end
   
   def create
@@ -18,7 +22,7 @@ class OrdersController < ApplicationController
     @sitter = session[:current_sitter].to_h
     @drop = Date.strptime(session[:drop_off], '%m/%d/%Y')
     @pick = Date.strptime(session[:pick_up], '%m/%d/%Y')
-    
+    @total = (@pick - @drop).to_i * @sitter['price']
     # @booking_date = BookingDate.new
     @booking_date_drop = BookingDate.new
     @booking_date_pick = BookingDate.new
@@ -27,10 +31,20 @@ class OrdersController < ApplicationController
     #[].push {date: @drop+1}
 
     if @order.save
+      nonce = params[:payment_method_nonce]
+      result = gateway.transaction.sale(
+        amount: @total,
+        payment_method_nonce: nonce,
+        order_id: @order.id,
+        options: {
+          submit_for_settlement: true
+        }
+      )
       # @booking_date.update(sitter_id: @sitter['id'])
       @booking_date_drop.update(sitter_id: @sitter['id'], date: @drop, available: false)
       @booking_date_pick.update(sitter_id: @sitter['id'], date: @pick, available: false)
-      
+     
+     
       # BookingDate.where(sitter_id: @sitter['id']).update(date: @drop..@pick, :available => false)
       redirect_to user_orders_path, notice:'成功下訂！'
     else
@@ -61,4 +75,6 @@ class OrdersController < ApplicationController
   # def booking_date_params
   #   params.require(:booking_date).permit(:sitter_id, :date, :avaliable)
   # end
+
+  
 end
