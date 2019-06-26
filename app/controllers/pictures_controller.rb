aee2b7d00d3c9f834b57a35de1654128dc9ffeac
Eric Syu <xeriok02390@gmail.com>
2019-06-26 10:12:47 +0800

    圖片上傳功能

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,13 +10,18 @@ class PicturesController < ApplicationController
   end
   def create
     @picture = Picture.new(picture_params)
-    
-    byebug
+    @current_sitter = Sitter.find_by("name == '#{current_user.name}'")
+    if @picture.save
+      redirect_to sitter_picture_path(params[:sitter_id],params[:sitter_id]), notice: '圖片上傳成功'
+    else
+      render :new
+    end
+    # byebug
   end
 
   private
   def picture_params
-    params.require(:picture).permit(:sitter_id, :pic)
+    params.require(:picture).permit(:sitter_id, :pic, :user_id)
     # params.require(:sitter).permit( :address, :slogan, :price, :square_meters, :pet_limit, :pic)
   end
 end
--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -13,7 +13,7 @@ class Sitter < ApplicationRecord
 
   
 
-  geocoded_by :address        #從address欄位取出地址
+  geocoded_by :address        #從add
   after_validation :geocode   #將取出的地址自動轉為經緯度分別存在 latitude、longitude 欄位
 
 end
