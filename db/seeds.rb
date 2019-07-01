c6dc15e80cc32a6ba56354770ee3cba10f127481
evansun <pipltbtb@gmail.com>
2019-07-01 19:30:44 +0800

    seed 同時建 sitter & user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
 file = File.open("address_six_country_test.txt")
 array = JSON.parse(file.read)
 
-# township_array =  ["臺北市中正區","臺北市大同區","臺北市中山區","臺北市松山區","臺北市大安區","臺北市萬華區","臺北市信義區","臺北市士林區","臺北市北投區","臺北市內湖區"]
-array.each do |address_six_country| 
+township_array =  ["臺北市中正區","臺北市大同區","臺北市中山區","臺北市松山區","臺北市大安區","臺北市萬華區","臺北市信義區","臺北市士林區","臺北市北投區","臺北市內湖區"]
+# array.each do |address_six_country| 
 
-# 5.times { 
-#     #sitter
+5.times { 
+    #sitter
     email = Faker::Internet.email
     name = Faker::Name.unique.name
     slogan = Faker::TvShows::GameOfThrones.quote
 
     price = Faker::Number.number(3)
-    avatar = "https://i.pravatar.cc/300?img=#{rand(1..50)}"
+    # avatar = "https://i.pravatar.cc/300?img=#{rand(1..50)}"
     number = rand(0..9)
     pet_limit = rand(1..5)
     square_meters = rand(20..50)
 
-    Sitter.create(name: name ,email: email, password: '123456', slogan:slogan, address:address_six_country, price:price, pet_limit: pet_limit,square_meters: square_meters)
+    Sitter.create(name: name ,email: email, password: '123456', slogan:slogan, address:"臺北市中正區", price:price, pet_limit: pet_limit,square_meters: square_meters)
     puts "."
-    User.create(name: name ,email: email, password: '123456', address:address_six_country, avatar:avatar,)
+    User.create(name: name ,email: email, password: '123456', address:"臺北市中正區")
     puts "."
     sitterid = Faker::Number.number(1)
-# }
-end
+}
+# end
 
 # 1.times { 
 #     #sitter
