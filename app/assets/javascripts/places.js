5fd496d818a872e4c972d90fb64a39427eeb59ee
LucaKao <lars10192002@gmail.com>
2019-07-03 13:48:40 +0800

    [Map] modify map style

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -58,88 +58,112 @@ function setMapStyle(data) {
   window.googleMapConfig.infowindow = new google.maps.InfoWindow()
 
   window.googleMapConfig.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
+    zoom: 15,
     center: { lat: data[0][1], lng: data[0][2] },
     styles: [
-      { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-      { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-      { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
       {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#d59563' }],
+          "featureType": "landscape",
+          "stylers": [
+              {
+                  "hue": "#FFA800"
+              },
+              {
+                  "saturation": 0
+              },
+              {
+                  "lightness": 0
+              },
+              {
+                  "gamma": 1
+              }
+          ]
       },
       {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#d59563' }],
+          "featureType": "road.highway",
+          "stylers": [
+              {
+                  "hue": "#53FF00"
+              },
+              {
+                  "saturation": -73
+              },
+              {
+                  "lightness": 40
+              },
+              {
+                  "gamma": 1
+              }
+          ]
       },
       {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{ color: '#263c3f' }],
+          "featureType": "road.arterial",
+          "stylers": [
+              {
+                  "hue": "#FBFF00"
+              },
+              {
+                  "saturation": 0
+              },
+              {
+                  "lightness": 0
+              },
+              {
+                  "gamma": 1
+              }
+          ]
       },
       {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#6b9a76' }],
+          "featureType": "road.local",
+          "stylers": [
+              {
+                  "hue": "#00FFFD"
+              },
+              {
+                  "saturation": 0
+              },
+              {
+                  "lightness": 30
+              },
+              {
+                  "gamma": 1
+              }
+          ]
       },
       {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{ color: '#38414e' }],
+          "featureType": "water",
+          "stylers": [
+              {
+                  "hue": "#00BFFF"
+              },
+              {
+                  "saturation": 6
+              },
+              {
+                  "lightness": 8
+              },
+              {
+                  "gamma": 1
+              }
+          ]
       },
       {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{ color: '#212a37' }],
-      },
-      {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#9ca5b3' }],
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{ color: '#746855' }],
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{ color: '#1f2835' }],
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#f3d19c' }],
-      },
-      {
-        featureType: 'transit',
-        elementType: 'geometry',
-        stylers: [{ color: '#2f3948' }],
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#d59563' }],
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{ color: '#17263c' }],
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#515c6d' }],
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{ color: '#17263c' }],
-      },
-    ],
+          "featureType": "poi",
+          "stylers": [
+              {
+                  "hue": "#679714"
+              },
+              {
+                  "saturation": 33.4
+              },
+              {
+                  "lightness": -25.4
+              },
+              {
+                  "gamma": 1
+              }
+          ]
+      }
+  ],
   })
 }
 
